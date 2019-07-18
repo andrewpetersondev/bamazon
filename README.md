7ab4be18b39f0b3c6b66d6b9e02f96e44189f87b
Andrew P <andrewpetersondev@gmail.com>
2019-07-18 17:40:34 -0500

    added calculateTotal function

--- README.md
+++ README.md
@@ -23,15 +23,17 @@ The app should then prompt users with two messages.
 The first should ask them the ID of the product they would like to buy.
 The second message should ask how many units of the product they would like to buy.
 
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+
 
 If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
 
 However, if your store does have enough of the product, you should fulfill the customer's order.
 
 This means updating the SQL database to reflect the remaining quantity.
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
 Once the update goes through, show the customer the total cost of their purchase.
 
 If this activity took you between 8-10 hours, then you've put enough time into this assignment. Feel free to stop here -- unless you want to take on the next challenge.
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,6 @@
+// configuration
+// ==========================================================================================================================
+
 // import packages
 const mysql = require("mysql");
 const inquirer = require("inquirer");
@@ -13,14 +16,18 @@ var connection = mysql.createConnection({
 });
 
 // connect to mysql server and database
-connection.connect(function (err) {
-    if (err) throw err;
+connection.connect(function (error) {
+    if (error) throw error;
     console.log("Connected as ID: " + connection.threadId + "\n");
-    // run a function that shows all of the inventory
-    // showInventory();
     start();
 });
 
+// global variables
+// ==========================================================================================================================
+var chosenItem = {};
+
+// functions
+// ==========================================================================================================================
 function start() {
     var query = "SELECT * FROM bamazon.products";
     connection.query(query, function (error, inventory) {
@@ -30,12 +37,24 @@ function start() {
             {
                 name: "item",
                 type: "number",
-                message: "What is the item_id of the item you would like to order?"
+                message: "What is the item_id of the item you would like to order?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
             },
             {
                 name: "units",
                 type: "number",
-                message: "How many units of the item would you like to order?"
+                message: "How many units of the item would you like to order?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
             }
         ]).then(function (userInput) {
             // var item = parseInt(userInput.item);
@@ -48,38 +67,31 @@ function start() {
 }
 
 function checkInventory(userInput) {
-    // console.log("checkInventory() called");
-
     var query = "SELECT * FROM `products` WHERE `item_id` = ?";
-
     connection.query(query, [userInput.item], function (error, itemInventory) {
-
         if (error) throw error;
-
         // for (var i = 0; i < itemInventory.length; i++) {
         //     console.log(itemInventory[i].stock_quantity);
         // }
-
         // console.log(itemInventory[0].stock_quantity);
-
-        if (itemInventory[0].stock_quantity > userInput.units) {
+        if (itemInventory[0].stock_quantity >= userInput.units) {
             console.log("we have enough in stock");
             purchase(userInput, itemInventory);
-
-
         } else {
             console.log("we don't have enough inventory to complete your order");
         }
-
     })
     // parseInt and make sure it is a number
-    // if else make comparison to check quantity
-    // makepurchase(product, quantity) in else statement 
-
 }
 
 function purchase(userInput, itemInventory) {
-
+    chosenItem = {
+        item_id: itemInventory[0].item_id,
+        product_name: itemInventory[0].product_name,
+        department_name: itemInventory[0].department_name,
+        price: itemInventory[0].price,
+        stock_quantity: itemInventory[0].stock_quantity
+    };
     var query = "UPDATE products SET ? WHERE ?";
     connection.query(query,
         [
@@ -91,11 +103,15 @@ function purchase(userInput, itemInventory) {
             }
         ], function (error, results) {
             if (error) throw error;
-            console.log(results);
-            console.log("hi");
+            // console.log(results);
         }
     )
-    // update database
-    // var query = /////
+    calculateTotal(userInput, itemInventory);
+}
+
+function calculateTotal(userInput) {
+    // console.log(chosenItem.price * userInput.units);
+    return chosenItem.price * userInput.units;
+
     connection.end();
 }
