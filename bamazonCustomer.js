47fc27ac5451de1c0c879f4e7694e049dc3cbfa3
Andrew P <andrewpetersondev@gmail.com>
2019-07-18 12:57:01 -0500

    updates

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -25,44 +25,53 @@ function start() {
 
     var query = "SELECT * FROM bamazon.products";
 
-    connection.query(query, function (error, results) {
+    connection.query(query, function (error, inventory) {
 
         if (error) throw error;
 
-        console.table(results);
+        console.table(inventory);
 
         inquirer.prompt([
             {
                 name: "item",
                 type: "number",
-                message: "What is the item_id of the item you would like to buy?"
+                message: "What is the item_id of the item you would like to order?"
             },
             {
                 name: "units",
                 type: "number",
-                message: "How many units of the item would you like to purchase?"
+                message: "How many units of the item would you like to order?"
             }
-        ]).then(function (answer) {
-            // console.log(answer);
-            var item = answer.item;
-            var units = answer.units;
-            // console.log(item, units);
-            checkOrder(units);
-            // 
-        })
-        connection.end();
+        ]).then(function (userInput) {
+           
+            var item = userInput.item;
+            var units = userInput.units;
+            
+            console.log(userInput);
+            console.log(item, units);
 
+            checkInventory(inventory, userInput);
+        })
     });
 }
 
-function checkOrder(results, answer) {
+function checkInventory(inventory, userInput) {
+    var query = "SELECT * FROM bamazon.products WHERE item_id = ?";
+    connection.query(query, [userInput.item], function (error, inventory) {
+        if (error) throw error;
+        console.log("checkInventory() called");
+        console.log(inventory);
+        var quantityInStock = inventory.stock_quantity;
+        console.log(quantityInStock);
+    })
     // parseInt and make sure it is a number
     // if else make comparison to check quantity
     // makepurchase(product, quantity) in else statement 
+    connection.end();
 }
 
-function makePurchase() {
-    // update database
-    // var query = /////
+// function makePurchase() {
+//     // update database
+//     // var query = /////
 
-}
+// }
